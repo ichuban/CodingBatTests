fbb4542b5e4a7e58aaf66e28bd171998a0934a5f
Rafal Suchodola <rsuchodola@gmail.com>
2018-10-19 10:20:57 +0200

    FirstTestSolved

---
+++ src/main/java/main.java
@@ -0,0 +1,20 @@
+/*
+Given an array of strings, return the count of the number of strings with the given length.
+wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+*/
+
+
+
+public class main {
+
+    public static void main( String[] args )
+    {
+        wordsCount wordsCount = new wordsCount();
+
+        System.out.println(wordsCount.stringLength(1));
+        System.out.println(wordsCount.stringLength(3));
+        System.out.println(wordsCount.stringLength(4));
+    }
+}
---
+++ src/main/java/wordsCount.java
@@ -0,0 +1,19 @@
+public class wordsCount {
+
+    private String[] wordsCount = new String[]{"a", "bb", "b", "ccc"};
+
+
+    public int stringLength(int stringLength) {
+
+        int count = 0;
+
+        for (String s : wordsCount) {
+            if (s.length() == stringLength) {
+
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
